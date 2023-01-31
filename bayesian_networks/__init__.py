195688a36510acd938ca527b0d993d6a2acb3e14
Luna Jiménez Fernández <luna.jimfer@gmail.com>
2023-01-31 11:30:01 +0100

    Re-structured BN definitions to include architecture and data

---
+++ bayesian_networks/__init__.py
@@ -0,0 +1,13 @@
+# BAYESIAN NETWORK - NN GS SPEEDUP #
+# Developed by Luna Jimenez Fernandez
+# Based on the work of Wenfeng Zhang et al.
+
+"""
+This module contains problem-agnostic definitions of **Bayesian Networks**, including:
+    - Bayesian Network architectures (DAGs)
+    - Bayesian Network data (raw data for each variable)
+
+These definitions are to be used by the rest of the project
+"""
+
+from .bn_architectures import BaseBN
---
+++ bayesian_networks/bn_architectures/__init__.py
@@ -0,0 +1,12 @@
+# BAYESIAN NETWORK - NN GS SPEEDUP #
+# Developed by Luna Jimenez Fernandez
+# Based on the work of Wenfeng Zhang et al.
+
+"""
+This module contains problem-agnostic definitions of **Bayesian Network architectures** (DAGs), containing
+all the edges (and their directions) between the variables.
+
+These definitions are to be used by the rest of the project
+"""
+
+from base_bn import BaseBN
