56703f2267392fa6b58775ed93cf8f544e077df6
Luna Jiménez Fernández <luna.jimfer@gmail.com>
2023-03-29 23:44:46 +0200

    Added initial Node Adjacency List creation logic

--- dag_architectures/node_adjacency_dag.py
+++ dag_architectures/node_adjacency_dag.py
@@ -4,9 +4,10 @@
 
 # IMPORTS #
 from typing import List
-
 from extended_dag import ExtendedDAG
+
 import numpy as np
+import networkx as nx
 import torch
 
 
@@ -41,7 +42,7 @@ class NodeAdjacencyDAG(ExtendedDAG):
 
     # List of arrays representing the parents of each variable / node
     # These lists are updated in size every time a node (or a list of nodes) are added or removed from the DAG
-    node_adjacency_list: List[np.ndarray]
+    _node_adjacency_list: List[np.ndarray]
 
     # CONSTRUCTOR #
 
@@ -71,7 +72,134 @@ class NodeAdjacencyDAG(ExtendedDAG):
             If specified, the following nodes will be removed from the node adjacency list instead
         """
 
+        # Check if the nodes list needs to be built for the first time
+        if self._node_adjacency_list is None:
+
+            # Create the initial list
+            self._node_adjacency_list = []
+
+            # Add each node to the list
+            for node_id in range(len(self)):
+
+                # Create the list and specify the node itself
+                new_node = np.zeros(len(self))
+                new_node[node_id] = -1
+
+                self._node_adjacency_list.append(new_node)
+
+        # Check if nodes have been removed from the matrix
+        elif removed:
+
+            # Remove the nodes from the node adjacency list
+            # (nodes are removed first to avoid extra work)
+            for node_id in removed:
+                del self._node_adjacency_list[node_id]
+
+            # For each remaining node, remove the appropriate indices
+            for adjacency_list in self._node_adjacency_list:
+                for node_id in removed:
+                    del adjacency_list[node_id]
+
+        # Otherwise, add additional nodes as required
+        else:
+
+            # Find the size difference between the original list and the new list
+            current_size = len(self._node_adjacency_list)
+            expected_size = len(self)
+
+            size_to_increase = expected_size - current_size
+
+            # FAILSAFE: Operate only if the size has increased
+            if size_to_increase > 0:
+
+                # Add the node to the already existing adjacency lists
+                for index in range(len(self._node_adjacency_list)):
+                    self._node_adjacency_list[index] = np.append(self._node_adjacency_list[index], [0] * size_to_increase)
+
+                # Add the new nodes
+                for node_index in range(size_to_increase):
+
+                    # Create the new adjacency list and specify the node itself
+                    new_node = np.zeros(expected_size)
+                    new_node[-(size_to_increase - node_index)] = -1
+
+    def get_node_adjacency_list(self, node):
+        """
+        Returns the node adjacency list for a specific node
+
+        Parameters
+        ----------
+        node: str or int
+            The node to get the node adjacency list from
+
+        Returns
+        -------
+        np.ndarray
+
+        Raises
+        ------
+        NetworkXError
+           If n is not in the graph.
+        TypeError
+            If the node is neither an int or a string
+        """
+
+        # Ensure that the input is either an int or a string
+        if not isinstance(node, (int, str)):
+            raise TypeError("Nodes must be either strings or integers")
+
+        # If node is a string name, get the appropriate node index
+        # Otherwise, raise a NetworkXError
+        try:
+            if isinstance(node, str):
+                node = self.node_to_index[node]
+        except KeyError:
+            raise nx.NetworkXError("There is no node with name {} in the graph".format(node))
+
+        # Return the node adjacency list
+        return self._node_adjacency_list[node]
+
+    def get_node_adjacency_list_tensor(self, node):
+        """
+        Returns the node adjacency list for a specific node as a tensor prepared for use as a neural network input
+
+        Parameters
+        ----------
+        node: str or int
+            The node to get the node adjacency list from
+
+        Returns
+        -------
+        torch.Tensor
+
+        Raises
+        ------
+        NetworkXError
+           If n is not in the graph.
+        TypeError
+            If the node is neither an int or a string
+        """
+
+        # Ensure that the input is either an int or a string
+        if not isinstance(node, (int, str)):
+            raise TypeError("Nodes must be either strings or integers")
+
+        # If node is a string name, get the appropriate node index
+        # Otherwise, raise a NetworkXError
+        try:
+            if isinstance(node, str):
+                node = self.node_to_index[node]
+        except KeyError:
+            raise nx.NetworkXError("There is no node with name {} in the graph".format(node))
+
+        # Return the node adjacency list
+        return torch.from_numpy(self._node_adjacency_list[node])
+
+    # NODE MANIPULATION #
+    # TODO - Add node, add nodes from, remove node, remove nodes from
     # EDGE MANIPULATION #
+    # TODO - Add edges from
+    # TODO - Fix methods to remove initialized
 
     def add_edge(self, u, v, weight=None):
         """
@@ -97,7 +225,7 @@ class NodeAdjacencyDAG(ExtendedDAG):
             super().add_edge(u, v, weight)
 
             # Add the edge to the adjacency matrix
-            self.node_adjacency_list[self.variable_index_dict[v]][self.variable_index_dict[u]] = 1
+            self._node_adjacency_list[self.variable_index_dict[v]][self.variable_index_dict[u]] = 1
 
         else:
             raise ValueError
@@ -123,7 +251,7 @@ class NodeAdjacencyDAG(ExtendedDAG):
             super().add_edge(u, v)
 
             # Remove the edge from the adjacency matrix
-            self.node_adjacency_list[self.variable_index_dict[v]][self.variable_index_dict[u]] = 0
+            self._node_adjacency_list[self.variable_index_dict[v]][self.variable_index_dict[u]] = 0
 
         else:
             raise ValueError
@@ -151,32 +279,8 @@ class NodeAdjacencyDAG(ExtendedDAG):
             super().invert_edge(u, v, weight)
 
             # Invert the edge in the adjacency matrix
-            self.node_adjacency_list[self.variable_index_dict[v]][self.variable_index_dict[u]] = 0
-            self.node_adjacency_list[self.variable_index_dict[u]][self.variable_index_dict[v]] = 1
+            self._node_adjacency_list[self.variable_index_dict[v]][self.variable_index_dict[u]] = 0
+            self._node_adjacency_list[self.variable_index_dict[u]][self.variable_index_dict[v]] = 1
 
         else:
             raise ValueError
-
-    # NEURAL NETWORK UTILITIES
-
-    def get_tensor(self, variable):
-        """
-        Returns the adjacency list of a specific variable as a tensor prepared for use as a neural network input
-
-        Parameters
-        ----------
-        variable : str or int
-            Variable to get the adjacency list of
-
-        Returns
-        -------
-        torch.Tensor
-        """
-
-        # Get the appropriate list depending on whether a string or an int is provided
-        if isinstance(variable, str):
-            adjacency = self.node_adjacency_list[self.variable_index_dict[variable]]
-        else:
-            adjacency = self.node_adjacency_list[variable]
-
-        return torch.from_numpy(adjacency)
