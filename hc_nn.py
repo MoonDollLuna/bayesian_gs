f0cc8ba5adfe32f76180bd97838aa790624b0a0b
Luna Jiménez Fernández <luna.jimfer@gmail.com>
2023-05-08 14:06:15 +0200

    Changed testing database

--- hc_nn.py
+++ hc_nn.py
@@ -6,6 +6,7 @@
 from pgmpy.readwrite.BIF import BIFReader
 from pgmpy.sampling import BayesianModelSampling
 from pgmpy.estimators import HillClimbSearch
+import time
 # TODO MOVE DATA SAMPLING TO WITHIN THE CLASS
 
 from dag_learning import HillClimbing
@@ -13,17 +14,18 @@ from dag_learning import HillClimbing
 # TODO ADD PROPER ARGUMENT PARSING
 
 # Parse an example BN
-BIF = BIFReader("./input/bif/small/asia.bif")
+BIF = BIFReader("./input/bif/large/hepar2.bif")
 model = BIF.get_model()
 
-# Sample an example amount of data (10000 data)
+# Sample an example amount of data (1000 data)
 data = BayesianModelSampling(model).forward_sample(size=10000)
 
 # Create and launch the model (own)
-hill_climbing = HillClimbing(model, list(model.nodes), data)
-dag = hill_climbing.estimate_dag(verbose=5)
+# hill_climbing = HillClimbing(model, list(model.nodes), data)
+# dag = hill_climbing.estimate_dag(verbose=2)
 
 # Create and launch the model (PGMPY)
+time = time.time()
 hill_climbing_pgmpy = HillClimbSearch(data)
 dag_pgmpy = hill_climbing_pgmpy.estimate(scoring_method="bdeuscore")
-dag_pgmpy.to_daft().show()
+print(time)
