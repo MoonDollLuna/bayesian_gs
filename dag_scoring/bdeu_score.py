1131b1e854a52b5dcf8e4ae1294b905a787db8dd
Luna Jiménez Fernández <luna.jimfer@gmail.com>
2023-09-21 14:17:51 +0200

    Modified the implementation back from numpy to pandas to compare speeds

--- dag_scoring/bdeu_score.py
+++ dag_scoring/bdeu_score.py
@@ -7,9 +7,11 @@ import math
 from itertools import product
 from functools import lru_cache
 
+import pandas as pd
 import numpy as np
 from scipy.special import gammaln
 from pgmpy.base import DAG
+
 
 # TODO - Make a generic Scorer class for the methods that are required
 class BDeuScore:
@@ -21,36 +23,28 @@ class BDeuScore:
     score is, in addition, decomposable - meaning that differences in the local scores for each variable
     can be re-computed instead of having to compute the full score.
 
-    This class is re-defined to speed up the implementation compared to pgmpy by using numpy vectorization.
-    This class also assumes that no missing data will be provided.
+    This class is re-defined to speed up the implementation compared to pgmpy by using:
+
+    - numpy vectorization
+    - lru_cache memoizing
+    - pandas speed-ups
+
+    It is also assumed that there will be no missing values.
 
     Parameters
     ----------
-    data: np.ndarray
-        The data over which the BDeU score will be computed. This data must be given as a 2D numpy Array where
+    data: pd.DataFrame
+        The data over which the BDeU score will be computed. This data must be given as a pandas DataFrame where
         each row represents an instance of the dataset, with each column representing a variable (following
         the order of nodes)
-    nodes: list[str]
-        List of ordered names of all variables.
     equivalent_sample_size: int, default=10
         Equivalent sample size for the BDeu score computation.
-    count_method: {"forloop", "unique", "mask"}, default="unique"
-        Method used to count state frequencies. Possible values:
-
-            * "forloop": Standard for loop
-            * "unique": np.unique over the sliced dataset
-            * "mask": Masking to segment the dataset into smaller datasets with each parent state combinations
     """
     # ATTRIBUTES #
 
     # DATA HANDLING #
     # Data contained within the scorer
-    data: np.ndarray
-    # Number of rows within the data, for convenience
-    data_rows_count: int
-    # Index of all nodes within the numpy array
-    # Shape: {variable_name: index}
-    node_index: dict
+    data: pd.DataFrame
     # Possible values for each node
     # Shape: {variable_name: (value_1, value_2...)}
     node_values: dict
@@ -58,46 +52,18 @@ class BDeuScore:
     # SCORE COMPUTING #
     # Equivalent Sample Size
     esz: int
-    # Counting method
-    count_method: str
 
     # CONSTRUCTOR AND INITIALIZATION METHODS #
-    def __init__(self, data, nodes, equivalent_sample_size=10, count_method="unique"):
+    def __init__(self, data, equivalent_sample_size=10):
 
-        # Store the input data and initialize the dictionaries
+        # Store the input data and initialize the dictionary
         self.data = data
-        self.data_rows_count = data.shape[0]
-        self._initialize_dictionaries(nodes)
+
+        # Initialize the dictionary using dictionary comprehension
+        self.node_values = {variable: list(self.data[variable].unique()) for variable in list(self.data.columns)}
 
         # Store the equivalent sample size
         self.esz = equivalent_sample_size
-
-        # Store the count method
-        self.count_method = count_method
-
-    def _initialize_dictionaries(self, variable_names):
-        """
-        Given a list of the variable names, initializes the internal dictionaries
-        for faster lookup.
-
-        Parameters
-        ----------
-        variable_names: list[str]
-            Ordered list of the variables contained within the data
-        """
-
-        # Initialize both dictionaries
-        self.node_index = {}
-        self.node_values = {}
-
-        # Get the index and name of each variable
-        for index, name in enumerate(variable_names):
-
-            # Store the index
-            self.node_index[name] = index
-
-            # Get the unique values in the column
-            self.node_values[name] = tuple(np.unique(self.data[:, index]).tolist())
 
     # SCORE FUNCTIONS #
 
@@ -292,6 +258,56 @@ class BDeuScore:
         return new_score - original_score
 
     # COUNT FUNCTIONS #
+
+    def _get_state_counts(self, variable, variable_states, parents, parent_state_combinations):
+        """
+        For each combination of parent states, returns the count of each variable state.
+
+        This method utilizes masks to filter the numpy array.
+
+        Parameters
+        ----------
+        variable : str
+            Variable of which the states are counted
+        variable_states : list[str]
+            List of all states of the variable
+        parents : list[str]
+            List of parents of the variable
+        parent_state_combinations : list[tuple[str]]
+            List of all possible combination of parent variables' states
+
+        Returns
+        -------
+        np.ndarray
+            Array where each row represents a state of the variable, each column represents
+            a combination of parent variable states and each cell represents the count of said
+            variable state for the given parents
+        """
+
+        # Initialize the numpy array to be returned
+        counts_array = np.zeros((len(variable_states), len(parent_state_combinations)))
+
+        # Generate dictionaries to know the index of each element (both variable and parent combination)
+        variable_states_dict = {state: index for index, state in enumerate(variable_states)}
+        parent_states_dict = {state_combination: index
+                              for index, state_combination
+                              in enumerate(parent_state_combinations)}
+
+        # Count the occurrences of each combination of variable and parent states using pandas value_counts
+        # Dropna is set to false to avoid looking for NaNs
+        subset = [variable] + parents
+        counts = self.data.value_counts(subset=subset, dropna=False)
+
+        # Move the counts into the count array
+        # Row: index of the original variable state (first element of each index)
+        # Column: index of the parents state combination (rest of the elements of each index)
+        # Count: number of instances of said combination
+        for row, column, count in [(variable_states_dict[comb[0]],
+                                    parent_states_dict[comb[:1]],
+                                    counts[comb]) for comb in counts.index]:
+
+            # Update the count
+            counts_array[row, column] = count
 
     def _get_state_counts_unique(self, variable, variable_states, parents, parent_state_combinations):
         """
