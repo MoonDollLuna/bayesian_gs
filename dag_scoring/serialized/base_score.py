daf9712b4e1b58c648747ba430e300bd1ec795c5
Luna Jiménez Fernández <luna.jimfer@gmail.com>
2023-11-06 12:59:29 +0100

    Updated BaseScore to allow easier implementations of further scoring methods

--- dag_scoring/serialized/base_score.py
+++ dag_scoring/serialized/base_score.py
@@ -3,8 +3,10 @@
 # Based on the work of Wenfeng Zhang et al.
 
 # IMPORTS
-
 from functools import lru_cache
+
+import numpy as np
+import pandas as pd
 
 
 class BaseScore:
@@ -12,10 +14,38 @@ class BaseScore:
     Abstract base class for serialized DAG and Bayesian Network structure scoring.
 
     All scoring serialized methods implemented (BDeu, BIC...) must extend this class and implement
-    the required methods.
+    the `local_score` method, using lru_cache for memoization.
 
-    This class assumes that Python's LRU cache is used for caching calls to local_score.
+    Other utility methods, such as `local_score_delta` or `global_score` are already provided and
+    reliant on `local_score` implementation.
+
+    Parameters
+    ----------
+    data: pd.DataFrame
+        The data over which the BDeU score will be computed. This data must be given as a pandas DataFrame where
+        each row represents an instance of the dataset, with each column representing a variable (following
+        the order of nodes)
     """
+
+    # DATA HANDLING #
+
+    # Data contained within the scorer
+    data: pd.DataFrame
+
+    # Possible values for each node
+    # Shape: {variable_name: (value_1, value_2...)}
+    node_values: dict
+
+    # CONSTRUCTOR AND INITIALIZATION METHODS #
+    def __init__(self, data):
+
+        # Store the input data and initialize the dictionary
+        self.data = data
+
+        # Initialize the dictionary using dictionary comprehension
+        self.node_values = {variable: list(self.data[variable].unique()) for variable in list(self.data.columns)}
+
+    # SCORING METHODS #
 
     @lru_cache(maxsize=None)
     def local_score(self, node, parents):
@@ -27,18 +57,111 @@ class BaseScore:
 
     def local_score_delta(self, node, original_parents, new_parents):
         """
-        Given a variable and two sets of parents (an original set and a new set),
-        returns the difference in local score between (node, new parents) and (node, original parents)
+        Given a node and the original and new set of parents (after an operation to add, remove
+        or invert and edge), computes the difference in local score between the new and old set of parents.
+
+        Parameters
+        ----------
+        node: str
+            The children node
+        original_parents: tuple[str]
+            The original parents of node
+        new_parents: tuple[str]
+            The new parents of node after the operation
+
+        Returns
+        -------
+        float
         """
 
-        raise NotImplementedError
+        # Compute the BDeu score for the original parents
+        original_score = self.local_score(node, original_parents)
+
+        # Compute the BDeu score for the new parents
+        new_score = self.local_score(node, new_parents)
+
+        return new_score - original_score
 
     def global_score(self, dag):
         """
-        Given a whole DAG, find the total score (the local score for each variable and set of parents)
+        Computes the global local score of the specified DAG. The global score of a DAG is
+        the sum of local scores for each variable and its parents.
+
+        Parameters
+        ----------
+        dag: DAG
+            A directed acyclic graph
+
+        Returns
+        -------
+        float
         """
 
-        raise NotImplementedError
+        score = 0.0
 
+        # For each variable, find its parents and compute the local score
+        for variable in list(dag.nodes):
+            parents = dag.get_parents(variable)
+            score += self.local_score(variable, tuple(parents))
 
+        return score
 
+    # UTILITY METHODS #
+
+    def _get_state_counts(self, variable, variable_states, parents, parent_state_combinations):
+        """
+        For each combination of parent states, returns the count of each variable state.
+
+        This method uses pandas' `value_counts` method to efficiently count the unique instances.
+
+        Parameters
+        ----------
+        variable : str
+            Variable of which the states are counted
+        variable_states : list[str]
+            List of all states of the variable
+        parents : list[str] or tuple[str]
+            List of parents of the variable
+        parent_state_combinations : list[tuple[str]]
+            List of all possible combination of parent variables' states
+
+        Returns
+        -------
+        np.ndarray
+            Array where each row represents a state of the variable, each column represents
+            a combination of parent variable states and each cell represents the count of said
+            variable state for the given parents
+        """
+
+        # Initialize the numpy array to be returned
+        counts_array = np.zeros((len(variable_states), len(parent_state_combinations)))
+
+        # Generate dictionaries to know the index of each element (both variable and parent combination)
+        variable_states_dict = {state: index for index, state in enumerate(variable_states)}
+        parent_states_dict = {state_combination: index
+                              for index, state_combination
+                              in enumerate(parent_state_combinations)}
+
+        # Count the occurrences of each combination of variable and parent states using pandas value_counts
+        # Dropna is set to False to avoid looking for NaNs
+        subset = [variable] + list(parents) if parents else variable
+        counts = self.data.value_counts(subset=subset, dropna=False)
+
+        # Move the counts into the count array
+        # Row: index of the original variable state (first element of each index)
+        # Column: index of the parents state combination (rest of the elements of each index)
+        # Count: number of instances of said combination
+
+        # Variables without parents have to be treated differently
+        if not parents:
+            for column, count in [(variable_states_dict[comb], counts[comb]) for comb in counts.index]:
+                # Update the count
+                counts_array[column] = count
+        else:
+            for row, column, count in [(variable_states_dict[comb[0]],
+                                        parent_states_dict[comb[1:]],
+                                        counts[comb]) for comb in counts.index]:
+                # Update the count
+                counts_array[row, column] = count
+
+        return counts_array
--- dag_scoring/serialized/bdeu_score.py
+++ dag_scoring/serialized/bdeu_score.py
@@ -7,10 +7,9 @@ import math
 from itertools import product
 from functools import lru_cache
 
-import pandas as pd
+from pandas import DataFrame
 import numpy as np
 from scipy.special import gammaln
-from pgmpy.base import DAG
 
 from dag_scoring import BaseScore
 
@@ -26,15 +25,14 @@ class BDeuScore(BaseScore):
 
     This class is re-defined to speed up the implementation compared to pgmpy by using:
 
-    - numpy vectorization
+    - numpy vectorization with pandas speed-ups
     - lru_cache memoizing
-    - pandas speed-ups
 
     It is also assumed that there will be no missing values.
 
     Parameters
     ----------
-    data: pd.DataFrame
+    data: DataFrame
         The data over which the BDeU score will be computed. This data must be given as a pandas DataFrame where
         each row represents an instance of the dataset, with each column representing a variable (following
         the order of nodes)
@@ -45,7 +43,7 @@ class BDeuScore(BaseScore):
 
     # DATA HANDLING #
     # Data contained within the scorer
-    data: pd.DataFrame
+    data: DataFrame
     # Possible values for each node
     # Shape: {variable_name: (value_1, value_2...)}
     node_values: dict
@@ -57,11 +55,8 @@ class BDeuScore(BaseScore):
     # CONSTRUCTOR AND INITIALIZATION METHODS #
     def __init__(self, data, equivalent_sample_size=10):
 
-        # Store the input data and initialize the dictionary
-        self.data = data
-
-        # Initialize the dictionary using dictionary comprehension
-        self.node_values = {variable: list(self.data[variable].unique()) for variable in list(self.data.columns)}
+        # Parent constructor call
+        super().__init__(data)
 
         # Store the equivalent sample size
         self.esz = equivalent_sample_size
@@ -165,59 +160,6 @@ class BDeuScore(BaseScore):
         final_value = prior_value + variable_value + parent_value
         return final_value
 
-    def global_score(self, dag):
-        """
-        Computes the global BDeu score of the specified DAG.
-
-        The global score of a DAG is the sum of local scores for each variable and its parents.
-
-        Parameters
-        ----------
-        dag: DAG
-            A directed acyclic graph
-
-        Returns
-        -------
-        float
-            Global BDeu score
-        """
-
-        score = 0.0
-
-        # For each variable, find its parents and compute the local score
-        for variable in list(dag.nodes):
-            parents = dag.get_parents(variable)
-            score += self.local_score(variable, tuple(parents))
-
-        return score
-
-    def local_score_delta(self, node, original_parents, new_parents, *args, **kwargs):
-        """
-        Given a node and the original and new set of parents (after an operation to add, remove
-        or invert and edge), computes the difference in local score between the new and old set of parents.
-
-        Parameters
-        ----------
-        node: str
-            The children node
-        original_parents: tuple[str]
-            The original parents of node
-        new_parents: tuple[str]
-            The new parents of node after the operation
-
-        Returns
-        -------
-        float
-        """
-
-        # Compute the BDeu score for the original parents
-        original_score = self.local_score(node, original_parents)
-
-        # Compute the BDeu score for the new parents
-        new_score = self.local_score(node, new_parents)
-
-        return new_score - original_score
-
     # UTILITY FUNCTIONS #
 
     def _get_local_prior_probability(self, parents_amount):
@@ -252,61 +194,3 @@ class BDeuScore(BaseScore):
         variable_likelihood = (sample_size - parents_amount) * math.log(1 - (structure_prior / sample_size))
 
         return dag_likelihood + variable_likelihood
-
-    def _get_state_counts(self, variable, variable_states, parents, parent_state_combinations):
-        """
-        For each combination of parent states, returns the count of each variable state.
-
-        This method utilizes masks to filter the numpy array.
-
-        Parameters
-        ----------
-        variable : str
-            Variable of which the states are counted
-        variable_states : list[str]
-            List of all states of the variable
-        parents : list[str] or tuple[str]
-            List of parents of the variable
-        parent_state_combinations : list[tuple[str]]
-            List of all possible combination of parent variables' states
-
-        Returns
-        -------
-        np.ndarray
-            Array where each row represents a state of the variable, each column represents
-            a combination of parent variable states and each cell represents the count of said
-            variable state for the given parents
-        """
-
-        # Initialize the numpy array to be returned
-        counts_array = np.zeros((len(variable_states), len(parent_state_combinations)))
-
-        # Generate dictionaries to know the index of each element (both variable and parent combination)
-        variable_states_dict = {state: index for index, state in enumerate(variable_states)}
-        parent_states_dict = {state_combination: index
-                              for index, state_combination
-                              in enumerate(parent_state_combinations)}
-
-        # Count the occurrences of each combination of variable and parent states using pandas value_counts
-        # Dropna is set to false to avoid looking for NaNs
-        subset = [variable] + list(parents) if parents else variable
-        counts = self.data.value_counts(subset=subset, dropna=False)
-
-        # Move the counts into the count array
-        # Row: index of the original variable state (first element of each index)
-        # Column: index of the parents state combination (rest of the elements of each index)
-        # Count: number of instances of said combination
-
-        # Variables without parents have to be treated differently
-        if not parents:
-            for column, count in [(variable_states_dict[comb], counts[comb]) for comb in counts.index]:
-                # Update the count
-                counts_array[column] = count
-        else:
-            for row, column, count in [(variable_states_dict[comb[0]],
-                                        parent_states_dict[comb[1:]],
-                                        counts[comb]) for comb in counts.index]:
-                # Update the count
-                counts_array[row, column] = count
-
-        return counts_array
